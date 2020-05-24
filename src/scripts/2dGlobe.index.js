4064ae8d537340d0a63df444e96a680563a057e7
W Li <levitabris@gmail.com>
2020-05-24 20:18:53 +0800

    Submitted version

---
+++ src/scripts/2dGlobe.index.js
@@ -0,0 +1,133 @@
+//lib
+import * as d3 from 'd3'
+import Globe from 'globe.gl'
+// import 'topojson-client'
+// import * as topojson from 'topojson-simplify'
+// import * as spo from 'svg-path-outline' TOO SlOW https://www.npmjs.com/package/svg-path-outline
+// import Offset from 'polygon-offset'
+
+// console.log(spo)
+
+// style sheet
+import '../styles/style.scss'
+// import '3dGlobe.js'
+
+// console.log(document.getElementById('log'))
+const print = (text) => (document.getElementById('log').innerText = text.toString())
+const parseDate = d3.utcParse('%d/%m/%Y')
+let tillTime = parseDate('31/12/2019')
+const updateInterval = 500
+
+// or https://cdn.jsdelivr.net/npm/globe.gl@2.10.1/example/datasets/ne_110m_admin_0_countries.geojson
+
+const sensitivity = 95
+
+let boxSize = d3.select('#world-2d').node().getBoundingClientRect()
+
+let projection = d3.geoOrthographic()
+    .scale(350)
+    .center([0, 0])
+    .rotate([-12, -60]) // points to northern europe
+    .translate([boxSize.width, boxSize.height / 2])
+
+const initialScale = projection.scale()
+let path = d3.geoPath().projection(projection)
+
+let svg = d3.select('#world-2d')
+            .append('svg')
+                .attr('class', 'countries')
+                .attr('width', '100%')
+                .attr('height', '100%')
+
+let globe = svg.append('circle')
+                .classed('globe-outline', true)
+                .attr('r', initialScale + 2)
+
+svg.call(d3.drag().on('drag', () => {
+    const rotate = projection.rotate()
+    const k = sensitivity / projection.scale()
+    projection.rotate([
+        rotate[0] + d3.event.dx * k,
+        rotate[1] - d3.event.dy * k
+    ])
+    path = d3.geoPath().projection(projection)
+    svg.selectAll('path').attr('d', path)
+}))
+
+fetch('/public/assets/world_map.json')
+    .then(res => res.json())
+    .then(countries => {
+        let defs = svg.append('g').attr('class', 'clip-defs')
+                        .selectAll('defs')
+                        .data(countries.features)
+                        .enter()
+                        .append('defs')
+
+        let paths = defs
+                    .append('path')
+                        .attr('class', d => 'country ' + d.properties.name.replace(' ', '_'))
+                        .attr('id', d => d.id)
+                        .attr('d', path)
+
+        let clips = defs
+                    .append('clipPath')
+                        .attr('id', x => `clip-${x.id}`)
+                        .append('use')
+                            .attr('xlink:href', x => `#${x.id}`)
+
+        let uses = svg.append('g')
+                        .attr('class', 'map')
+                        .selectAll('g')
+                        .data(countries.features)
+                        .enter()
+                        .append('g')
+                            .append('use')
+                            .attr('class', x => `use-origin use-origin-${x.id}`)
+                            .attr('xlink:href', x => `#${x.id}`)
+                            .attr('clip-path', x => `url(#clip-${x.id})`)
+
+        let uses_copy = uses.clone(true).attr('class', x => `use-copy use-copy-${x.id}`)
+                  
+        // print(countries.features.length)
+        fetch('public/assets/COVID19_geo_dist.csv')
+            .then(res => res.text())
+            .then(csv => d3.dsvFormat(';').parse(csv, ({ dateRep, deaths, cases, countryterritoryCode, popData2018 }) => 
+                ({
+                  deaths: Number(deaths)
+                , cases: Number(cases)
+                , popData2018: Number(popData2018)
+                , countryterritoryCode: countryterritoryCode.toString()
+                , dateRep: parseDate(dateRep)
+                })
+            ))
+            .then(reports => {
+                const drawLines = () => {
+                    paths.each(function (x) {
+                        let caseByPop = reports.filter(r => r.countryterritoryCode === x.id).filter(r => (r.dateRep - tillTime <= 0)).map(r => r.cases / r.popData2018).reduce((a, b) => (a + b), 0)
+                        let lineWidth = (Math.pow(caseByPop, .5) - 0.000) * 300        // if (!caseByPop) alert(x.id)
+                        d3.select(`.use-origin-${x.id}`).style('stroke-width', lineWidth * 1.5)//.style('stroke', caseByPop ? d3.interpolateYlOrRd(Math.pow(caseByPop, .3) * 4) : 'none')
+                        d3.select(`.use-copy-${x.id}`).style('stroke-width', lineWidth).style('stroke', caseByPop ? d3.interpolateYlOrRd(Math.pow(caseByPop, .3) * 4) : 'none')//
+                        // print(d3.select(`.use-copy-${x.id}`))
+                    })     
+                }
+
+                //Optional rotate
+                d3.timer(function (elapsed) {
+                    const rotate = projection.rotate()
+                    const k = sensitivity / projection.scale()
+                    projection.rotate([
+                        rotate[0] - 1 * k,
+                        rotate[1]
+                    ])
+                    path = d3.geoPath().projection(projection)
+                    svg.selectAll('path').attr("d", path)
+
+                    tillTime = 
+                        tillTime < parseDate('16/5/2020') ? new Date(+tillTime + sensitivity * 24 * 60 * 60 * 15) : parseDate('16/5/2020')
+                    drawLines()
+                    print(tillTime)
+                }, updateInterval)
+
+            })
+
+    })
---
+++ src/scripts/index.js
@@ -0,0 +1,160 @@
+//lib
+import Globe from 'globe.gl'
+import * as d3 from 'd3'
+// import Offset from 'polygon-offset'
+
+// style sheet
+import '../styles/style.scss'
+
+// console.log(document.getElementById('log'))
+const print = (text) => (document.getElementById('log').innerHTML = text)
+const parseDate = d3.utcParse('%d/%m/%Y')
+const tillTime = parseDate('16/5/2020')
+// or https://cdn.jsdelivr.net/npm/globe.gl@2.10.1/example/datasets/ne_110m_admin_0_countries.geojson
+
+Promise.all([
+    // fetch geojson world map
+    fetch('/public/assets/ne_110m_admin_0_countries.geojson') 
+        .then(res => res.json()), 
+    // fetch distributions statistic about covid-19
+    fetch('/public/assets/COVID19_geo_dist.csv') 
+        .then(res => res.text())
+        .then(csv => d3.dsvFormat(';').parse(csv, ({ dateRep, deaths, cases, countryterritoryCode, popData2018 }) =>
+            ({
+                  deaths: Number(deaths)
+                , cases: Number(cases)
+                , popData2018: Number(popData2018)
+                , countryterritoryCode: countryterritoryCode.toString()
+                , dateRep: parseDate(dateRep)
+            })
+        )),
+    // fetch paper author network
+    fetch('/public/assets/arcs_by_30.json')
+        .then(res => res.json())
+    ,
+    // fetch full paper info
+    fetch('/public/assets/geocoded_institutions.csv')
+        .then(res => res.text())
+        .then(csv => d3.dsvFormat(',').parse(csv))
+    // ,
+    // fetch 
+    // fetch('/public/assets/paper_author_loc.csv')
+    //     .then(res => res.text())
+    //     .then(csv => d3.dsvFormat(',').parse(csv))
+    ])
+    // end of Promise missions
+    .then(([countries, reports, arcs, institutions]) => {
+        // console.log(countries.features)
+
+
+        const MAP_CENTER = { lat: 34.3416, lng: 108.9398, altitude: 2.4 }
+        const OPACITY = .4
+
+        // return console.log(arcs.filter(x => x.arc.length != 1))
+        const world = Globe({
+                animateIn: true
+              , rendererConfig: { antialias: true, alpha: true }
+        })(document.getElementById('world-3d'))
+            .globeImageUrl('/public/assets/earth-dark.jpg')
+            .bumpImageUrl('/public/assets/earth-topology.png')
+            // .backgroundImageUrl('/public/assets/night-sky.png')
+            .pointOfView(MAP_CENTER, 0)
+
+            // .enablePointerInteraction(false)
+            .width(window.innerWidth * 1.2)
+            .height(window.innerHeight * 1.05)
+
+            .polygonsData(countries.features)
+                .polygonGeoJsonGeometry( feature => feature.geometry)
+                .polygonCapColor(() => 'rgba(0, 0, 0, 0.001)')
+                .polygonStrokeColor(() => 'rgba(234, 26, 41, 1)')
+                .polygonSideColor(() => 'rgba(193, 34, 84, .25)')
+                .polygonLabel(country => country.properties.SUBUNIT)
+                .polygonAltitude( country => {
+                    // console.log(country)
+                    let caseHere = reports
+                    .filter(
+                        r => ( r.dateRep - tillTime <= 0 )
+                    ).filter(
+                        r => ( r.countryterritoryCode === country.properties.ISO_A3)
+                    ).map( 
+                        r => r.cases / r.popData2018
+                    ).reduce( 
+                        (a, b) => (a + b)
+                        , 0
+                    )
+                return Math.pow(caseHere, .5) * 5
+                })
+                .onPolygonClick(country => {
+                    print(
+                        `<h5>Institutions in ${country.properties.SUBUNIT}</h5>
+                        <p>${institutions.filter(i => i.country.toString() !== 'NA').filter(i => i.country === country.properties.SUBUNIT).map(i => i.name).map(name => name).join('\n')}
+                        </p>
+                        `)
+                    // alert(world)
+                    world.arcColor((d) => d.countries.includes(country.properties.SUBUNIT) ? `rgba(0, 120, 240, ${OPACITY})` : `rgba(0, 120, 240, ${OPACITY / 6})`)
+                        .arcStroke((d) => d.countries.includes(country.properties.SUBUNIT) ? .5 : .1)
+                        // .arcDashAnimateTime(3500)
+                })
+
+
+            .arcsData(arcs)
+                // .arcLabel(d => d.paper_id)
+                // .arcCurveResolution(64)
+                .arcAltitudeAutoScale(.5)
+                // .arcCircularResolution(3)
+                .arcStartLat(d => Number(d.arc[0][0]))
+                .arcStartLng(d => Number(d.arc[0][1]))
+                .arcEndLat(d => Number(d.arc[1][0]))
+                .arcEndLng(d => Number(d.arc[1][1]))
+                .arcColor(() => `rgba(0, 120, 240, ${OPACITY})`)
+                .arcStroke(.2)
+                .arcDashLength(0.4)
+                .arcDashGap(0.2)
+                .arcDashAnimateTime(2000)
+                .arcLabel(d => `
+                    <h2 class="paper-title">${d.title}</h2>
+                    <p class="country-label">${(d.countries.length === 2 ? `${d.countries[0]} x ${d.countries[1]}` : d.countries[0])}</p>
+                `)
+            
+            .pointsData(institutions)
+                // .pointLabel(d => d.name)
+                .pointLat(d => +d.lat)
+                .pointLng(d => +d.lon)
+                .pointRadius(.3)
+                .pointAltitude(0)
+                .pointResolution(12)
+                .pointColor(() => `rgba(0, 120, 240, .8)`)
+            
+            
+            // .camera(cam => {
+            //     console.log(cam)
+            //     return cam.setViewOffset({
+            //         fullWidth: window.innerWidth
+            //         , fullHeight: window.innerHeight
+            //         , x: 0
+            //         , y: 0
+            //         , width: window.innerWidth - 20
+            //         , height: window.innerHeight - 20
+            //     })
+            // })
+            
+
+        // world.camera(cam => {
+        //         console.log(cam)
+        //         return cam.setViewOffset({
+        //               fullWidth: window.innerWidth + 700
+        //             , fullHeight: window.innerHeight + 700
+        //             , x: 0
+        //             , y: 0
+        //             , width: window.innerWidth 
+        //             , height: window.innerHeight 
+        //         })
+        // })
+
+        // Add auto-rotation
+        world.controls().autoRotate = true;
+        world.controls().autoRotateSpeed = 0.35;
+
+        print(`<h5>${institutions.length} Research Institutions Worldwide Joining Hands Against COVID-19</h5>`)
+    })
